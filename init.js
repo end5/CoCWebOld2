620fba4ca110c1c647d207e65a6c9ec026d5f277
end5 <16655171+end5@users.noreply.github.com>
2019-03-16 12:22:21 -0700

    Fix init script

--- init.js
+++ init.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 
-fs.exists('Build', (exists) => {
-    if (!exists)
-        fs.mkdir('Build', (err) => { if (err) throw err; });
-});
-fs.copyFile('node_modules/requirejs/require.js', 'Build/require.js', (err) => { if (err) throw err; });
-fs.exists('Build/codemirror', (exists) => {
-    if (!exists)
-        fs.mkdir('Build/codemirror', (err) => { if (err) throw err; });
-});
-fs.exists('Build/codemirror/lib', (exists) => {
-    if (!exists)
-        fs.mkdir('Build/codemirror/lib', (err) => { if (err) throw err; });
-});
-fs.exists('Build/codemirror/theme', (exists) => {
-    if (!exists)
-        fs.mkdir('Build/codemirror/theme', (err) => { if (err) throw err; });
-});
-fs.copyFile('node_modules/codemirror/lib/codemirror.css', 'Build/codemirror/lib/codemirror.css', (err) => { if (err) throw err; });
-fs.copyFile('node_modules/codemirror/lib/codemirror.js', 'Build/codemirror/lib/codemirror.js', (err) => { if (err) throw err; });
-fs.copyFile('node_modules/codemirror/theme/lesser-dark.css', 'Build/codemirror/theme/lesser-dark.css', (err) => { if (err) throw err; });
+if (!fs.existsSync('Build'))
+    fs.mkdirSync('Build');
+
+fs.copyFileSync('node_modules/requirejs/require.js', 'Build/require.js');
+
+if (!fs.existsSync('Build/codemirror'))
+    fs.mkdirSync('Build/codemirror');
+
+if (!fs.existsSync('Build/codemirror/lib'))
+    fs.mkdirSync('Build/codemirror/lib');
+
+fs.copyFileSync('node_modules/codemirror/lib/codemirror.css', 'Build/codemirror/lib/codemirror.css');
+fs.copyFileSync('node_modules/codemirror/lib/codemirror.js', 'Build/codemirror/lib/codemirror.js');
+
+if (!fs.existsSync('Build/codemirror/theme'))
+    fs.mkdirSync('Build/codemirror/theme');
+
+fs.copyFileSync('node_modules/codemirror/theme/lesser-dark.css', 'Build/codemirror/theme/lesser-dark.css');
